cc41a924ede134355b3014b360eda97a256a0335
Jonathan Croskell <15827290+joncros@users.noreply.github.com>
2019-01-20 16:34:38 -0700

    Removed QueryResult class from master (unsure if class will be used, so QueryResult development will be in QueryResult branch).

--- src/main/java/com/github/joncros/random_word/DatamuseWordService.java
+++ src/main/java/com/github/joncros/random_word/DatamuseWordService.java
@@ -1,17 +1,15 @@
 package com.github.joncros.random_word;
 
-import java.util.List;
-
 public class DatamuseWordService implements WordService {
 
     @Override
-    public QueryResult findWordsStartingWith(String s) {
+    public String[] findWordsStartingWith(String s) {
         //todo
         return null;
     }
 
     @Override
-    public QueryResult findWordsStartingWith(String s, int maxWordLength) {
+    public String[] findWordsStartingWith(String s, int maxWordLength) {
         //todo
         return null;
     }
--- src/main/java/com/github/joncros/random_word/WordService.java
+++ src/main/java/com/github/joncros/random_word/WordService.java
@@ -3,7 +3,7 @@ package com.github.joncros.random_word;
 import java.util.List;
 
 public interface WordService {
-    QueryResult findWordsStartingWith(String s);
+    String[] findWordsStartingWith(String s);
 
-    QueryResult findWordsStartingWith(String s, int maxWordLength);
+    String[] findWordsStartingWith(String s, int maxWordLength);
 }
