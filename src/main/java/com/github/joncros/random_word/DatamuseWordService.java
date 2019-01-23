a56bf8fce831a9c02e7ad5c27990f2a86b7f3dda
Jonathan Croskell <15827290+joncros@users.noreply.github.com>
2019-01-22 17:26:49 -0700

    Wordservice: changed parameter maxWordLength to maxLength. Implemented DatamuseWordService.

--- src/main/java/com/github/joncros/random_word/DatamuseWordService.java
+++ src/main/java/com/github/joncros/random_word/DatamuseWordService.java
@@ -1,18 +1,47 @@
 package com.github.joncros.random_word;
 
+import datamuse.DatamuseQuery;
+import datamuse.JSONParse;
+
 import java.util.List;
 
+/**
+ * Queries the Datamuse RESTful API for matching words
+ */
 public class DatamuseWordService implements WordService {
+    private DatamuseQuery datamuseQuery;
+    private JSONParse jsonParse;
 
-    @Override
-    public QueryResult findWordsStartingWith(String s) {
-        //todo
-        return null;
+    public DatamuseWordService() {
+        datamuseQuery = new DatamuseQuery(1000);
+        jsonParse = new JSONParse();
     }
 
+    /**
+     * Find words starting with a specified string
+     * @param s the letter(s) the words should start with
+     * @return a QueryResult holding the matching words
+     */
     @Override
-    public QueryResult findWordsStartingWith(String s, int maxWordLength) {
-        //todo
-        return null;
+    public QueryResult findWordsStartingWith(String s) {
+        //todo tests?
+        String resultString = datamuseQuery.wordsStartingWith(s);
+        String[] resultArray = jsonParse.parseWords(resultString);
+        return new QueryResult(resultArray);
+    }
+
+    /**
+     * Find words of a specific length starting with a specified string
+     * @param s the letter(s) the words should start with
+     * @param wordLength the length each matching word should be
+     * @return a QueryResult holding the matching words
+     */
+    @Override
+    public QueryResult findWordsStartingWith(String s, int wordLength) {
+        //todo tests?
+        int length = wordLength - s.length();
+        String resultString = datamuseQuery.wordsStartingWith(s, wordLength);
+        String[] resultArray = jsonParse.parseWords(resultString);
+        return new QueryResult(resultArray);
     }
 }
