c98d207e77220e70355fe2fbb6febf7420cc9d95
Jonathan Croskell <15827290+joncros@users.noreply.github.com>
2019-02-01 12:29:03 -0700

    Moved main method to new class Demo.

---
+++ src/main/java/com/github/joncros/random_word/Demo.java
@@ -0,0 +1,41 @@
+package com.github.joncros.random_word;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Demo {
+
+    private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+
+    public static void main(String[] args ) throws InterruptedException {
+        System.out.println("Chooses random letters until a complete word existing in a specific list of " +
+                "words is assembled.");
+        Scanner in = new Scanner(System.in);
+        // If a GUI is implemented, offer choice of CLI or GUI here
+        // If more than one WordService is implemented, offer choice of WordSevice here
+        System.out.println("Type the location of a text file containing a list of words," +
+                " or leave blank to use the Datamuse service:");
+        String choice = in.nextLine();
+        WordService service;
+        if (choice.isBlank()) {
+            service = new DatamuseWordService();
+        }
+        else {
+            service = new TextFileWordService(new File(choice));
+            //todo handle invalid File?
+        }
+
+        System.out.print("What is the minimum length of the word to be generated? ");
+        int minLength = in.nextInt();
+        System.out.print("What is the maximum length of the word to be generated? ");
+        int maxLength = in.nextInt();
+
+        RandomWord randomWord = new RandomWord(
+                minLength,
+                maxLength,
+                service,
+                new RandomLetterGenerator(ALPHABET),
+                new CLI());
+        randomWord.generateWord();
+    }
+}
--- src/main/java/com/github/joncros/random_word/RandomWord.java
+++ src/main/java/com/github/joncros/random_word/RandomWord.java
@@ -11,44 +11,11 @@ public class RandomWord {
     private RandomWordUI ui;
     private int minLength;
 	private int maxLength;
-	private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
 
     /**
      * If query result size is <= SMALL, next random letter is chosen from QueryResult.charsInNthPlace
       */
 	private final int SMALL = 20;
-
-    public static void main( String[] args ) throws InterruptedException {
-        System.out.println("Chooses random letters until a complete word existing in a specific list of " +
-                "words is assembled.");
-        Scanner in = new Scanner(System.in);
-        // If a GUI is implemented, offer choice of CLI or GUI here
-        // If more than one WordService is implemented, offer choice of WordSevice here
-        System.out.println("Type the location of a text file containing a list of words," +
-                " or leave blank to use the Datamuse service:");
-        String choice = in.nextLine();
-        WordService service;
-        if (choice.isBlank()) {
-            service = new DatamuseWordService();
-        }
-        else {
-            service = new TextFileWordService(new File(choice));
-            //todo handle invalid File?
-        }
-
-        System.out.print("What is the minimum length of the word to be generated? ");
-        int minLength = in.nextInt();
-        System.out.print("What is the maximum length of the word to be generated? ");
-        int maxLength = in.nextInt();
-
-        RandomWord randomWord = new RandomWord(
-                minLength,
-                maxLength,
-                service,
-                new RandomLetterGenerator(ALPHABET),
-                new CLI());
-        randomWord.generateWord();
-    }
 
     public RandomWord(int minLength, int maxLength, WordService wordService,
                       RandomLetterGenerator generator, RandomWordUI ui) {
