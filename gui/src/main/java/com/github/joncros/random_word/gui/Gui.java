41b474e11b2fc0cf5fe07b3a66e5a66d7f0b6d9c
Jonathan Croskell <15827290+joncros@users.noreply.github.com>
2019-02-11 16:30:07 -0700

    Implemented ComboBox functionality including FileChooser to choose a text file.

--- gui/src/main/java/com/github/joncros/random_word/gui/Gui.java
+++ gui/src/main/java/com/github/joncros/random_word/gui/Gui.java
@@ -2,6 +2,11 @@ package com.github.joncros.random_word.gui;
 
 import eu.hansolo.fx.spinner.*;
 import javafx.application.Application;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
@@ -12,8 +17,11 @@ import javafx.scene.control.Label;
 import javafx.scene.control.Spinner;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.util.StringConverter;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,13 +31,18 @@ public class Gui extends Application {
     private static final int NUM_SPINNERS_INITIAL = 10;
     private static final int MAX_WORD_LENGTH = 30;
 
+    private Scene scene;
+
     private List<CanvasSpinner> spinners;
 
     private Button generateButton;
     private EventHandler<ActionEvent> buttonEventHandler;
 
     private Label sourceLabel = new Label("Word Source");
-    private ChoiceBox<String> sourceChoiceBox;
+    private ChoiceBox<Object> sourceChoiceBox;
+    private ObjectProperty<Object> choiceBoxValueProperty;
+    private final Object DatamuseChoice = new Object();
+    private final Object TextfileChoice = new Object();
 
     private Label minLabel= new Label("Min Length");
     private Spinner<Integer> minLengthSpinner;
@@ -51,14 +64,42 @@ public class Gui extends Application {
 
         //create control to choose word source
         sourceChoiceBox = new ChoiceBox<>();
-        sourceChoiceBox.getItems().addAll("DataMuse", "Text File");
+        sourceChoiceBox.getItems().addAll(DatamuseChoice, TextfileChoice);
+        sourceChoiceBox.setConverter(new StringConverter<>() {
+            @Override
+            public String toString(Object o) {
+                if (o == DatamuseChoice) return "DataMuse";
+                else if (o == TextfileChoice) return "Text File";
+                else {
+                    File file = (File) o;
+                    return file.getName();
+                }
+            }
+
+            @Override
+            public Object fromString(String s) {
+                if (s.equals("DataMuse")) return DatamuseChoice;
+                else if (s.equals("Text File")) return TextfileChoice;
+                else {
+                    File file = new File(s);
+                    return file;
+                }
+            }
+        });
+        choiceBoxValueProperty = sourceChoiceBox.valueProperty();
+        choiceBoxValueProperty.addListener((observableValue, o, n) -> {
+            if (n == TextfileChoice) {
+                choiceBoxTextFileChosen();
+            }
+        });
+
 
         //create controls to choose word length
         minLengthSpinner = new Spinner<>(0, MAX_WORD_LENGTH, 5);
         minLengthSpinner.setMaxWidth(60);
         minLengthSpinner.setEditable(true);
 
-        maxLengthSpinner = new Spinner<>(2, MAX_WORD_LENGTH, 2);
+        maxLengthSpinner = new Spinner<>(2, MAX_WORD_LENGTH, 10);
         maxLengthSpinner.setMaxWidth(60);
         maxLengthSpinner.setEditable(true);
     }
@@ -77,7 +118,7 @@ public class Gui extends Application {
         vBox.setSpacing(10);
         spinnerBox.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
 
-        Scene scene = new Scene(vBox);
+        scene = new Scene(vBox);
         stage.setTitle("Generate Random Words");
         stage.setScene(scene);
         stage.show();
@@ -85,5 +126,27 @@ public class Gui extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    /*
+    Fired when the value "Text File" is chosen in the choice box.
+     */
+    private File choiceBoxTextFileChosen() {
+        //todo call this from buttonEventHandler if choice box value is "Text File?"
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open text file");
+        fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+        File selected = fileChooser.showOpenDialog(scene.getWindow());
+
+        if (selected != null) {
+            ObservableList<Object> items = sourceChoiceBox.getItems();
+            if ( !items.contains(selected) ) {
+                //file has not been added to combo box items
+                items.add(selected);
+            }
+            choiceBoxValueProperty.setValue(selected);
+        }
+
+        return selected;
     }
 }
